cdc46c67cdc316a7031c025ed8bb3f84406a941a
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-05-04 19:05:44 +0600

    version updated

--- src/main/java/com/lapsa/insurance/jpa/InsuranceVersion.java
+++ src/main/java/com/lapsa/insurance/jpa/InsuranceVersion.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "INSURANCE_VER_2_5")
+@Table(name = "INSURANCE_VER_2_6")
 @Access(AccessType.PROPERTY)
 public class InsuranceVersion {
 
