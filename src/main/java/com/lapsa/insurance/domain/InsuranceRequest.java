1965cda80a3f9ddce8ac4c852fc0d3e5eb9e1db7
Vadim.Isaev <vadim.o.isaev@gmail.com>
2019-01-05 13:11:09 +0300

    column TRANSACTION_STATUS renamed to CONTRACT_STATUS

--- src/main/java/com/lapsa/insurance/domain/InsuranceRequest.java
+++ src/main/java/com/lapsa/insurance/domain/InsuranceRequest.java
@@ -29,7 +29,7 @@ public abstract class InsuranceRequest extends Request {
 
     @Basic
     @Enumerated(EnumType.STRING)
-    @Column(name = "TRANSACTION_STATUS")
+    @Column(name = "CONTRACT_STATUS")
     protected ContractStatus contractStatus;
 
     @Basic
