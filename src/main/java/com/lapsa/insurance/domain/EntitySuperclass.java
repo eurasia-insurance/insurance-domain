2ee8b35d656abe0dbe1350e42dfbc70c8d921501
Vadim.Isaev <vadim.o.isaev@gmail.com>
2019-01-16 23:23:55 +0300

    no versioning

--- src/main/java/com/lapsa/insurance/domain/EntitySuperclass.java
+++ src/main/java/com/lapsa/insurance/domain/EntitySuperclass.java
@@ -1,6 +1,5 @@
 package com.lapsa.insurance.domain;
 
-import java.sql.Timestamp;
 import java.time.Instant;
 
 import javax.persistence.Basic;
@@ -9,10 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Version;
 
 import tech.lapsa.java.commons.function.MyNumbers;
-import tech.lapsa.java.commons.function.MyObjects;
 import tech.lapsa.java.commons.function.MyOptionals;
 
 @MappedSuperclass
@@ -41,29 +38,19 @@ public abstract class EntitySuperclass extends Domain {
 
     @Basic
     @Column(name = "UPDATED", nullable = false)
-    private Timestamp updated;
+    private Instant updated = Instant.now();
 
     public Instant getUpdated() {
-	return MyObjects.nullOrGet(updated, Timestamp::toInstant);
+	return updated;
+    }
+
+    public void touchUpdated() {
+	this.updated = Instant.now();
     }
 
     protected String appendEntityId() {
 	return appendEntityId(id);
     }
-
-
-//  @Basic
-//  @Column(name = "UPDATED", nullable = false)
-//  private Instant updated;
-//
-//  public Instant getUpdated() {
-//	return updated;
-//  }
-//
-//  public void touchUpdated() {
-//	this.updated = Instant.now();
-//  }
-
 
     protected static String appendEntityId(final Object id) {
 	return " [ID=" + MyOptionals.of(id).map(Object::toString).orElse("NONE") + "]";
